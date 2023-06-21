a86dc4204e77c439c0029c3f86e6431351e335b5
Colin Rennie <colinmatthew@gmail.com>
2023-06-21 18:24:30 -0400

    Mostly MVP

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ sqlite3 instance/db.sqlite
 .separator "," "\n"
 .import --skip 1 data/images.csv images
 ```
+
+### BUGS:
+* Some tall images are shown way too large e.g. http://127.0.0.1:8000/image/a72c20a0-1e07-40b8-9d44-a8b808572f6f
--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
+import random
 import sqlalchemy
 import uuid
 
 from flask import Blueprint, flash, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
-
 from sqlalchemy.sql.expression import cast
+from typing import Optional
 
 from . import db
 from .models import Images, UserImages
@@ -13,46 +14,77 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
+    """
+    Route for base splash page.
+    """
     return render_template("index.html", current_user=current_user)
 
 @main.route("/profile")
 @login_required
 def profile():
-    user_images = db.session.query(
-        UserImages, Images, UserImages
-        ).filter(cast(Images.uuid, sqlalchemy.String) == cast(UserImages.image_uuid, sqlalchemy.String)
-                 ).order_by(UserImages.timestamp.asc()).all()
+    """
+    Route to view profile of annotated images for this user.
+    """
+    user_images = db.session.query(UserImages.user_id, UserImages.timestamp, UserImages.image_uuid, UserImages.annotation, Images.thumbnail_url
+                    ).filter(Images.uuid == UserImages.image_uuid
+                    ).filter(UserImages.user_id == current_user.id
+                    ).order_by(UserImages.timestamp.asc()
+                    ).all()
     return render_template("profile.html", current_user=current_user, user_images=user_images)
 
 @main.route("/image", methods=["GET"])
 def new_image():
+    """
+    Route to fetch a random new image to caption.
+    
+    Note: For authenticated users, this will not overlap with images they've already annotated.
+    """
+    all_images = {y[0] for y in db.session.query(Images.uuid).all()}
+    
+    if current_user.is_authenticated:
+        completed_images = {x[0] for x in db.session.query(UserImages.image_uuid).filter(UserImages.user_id == current_user.id).all()}
 
-    # TODO: implement logic for selecting randomly without overlap of existing user images
-    # if current_user.is_authenticated:
-    #     image = db.session.query(
-    #         Images, UserImages
-    #                 ).filter(UserImages.user_id == current_user.id
-    #                 ).filter(
-    #                 )
-    image = Images.query.first()
+        image_uuid = random.sample(all_images.difference(completed_images), 1)[0]
+    else:
+        image_uuid = random.sample(all_images, 1)[0]
 
-    return redirect(url_for("main.image", image_uuid=image.uuid))
+    return redirect(url_for("main.image", image_uuid=image_uuid))
 
 
 @main.route("/image/<image_uuid>", )
-def image(image_uuid=None):
+def image(image_uuid: str = None):
+    """
+    Route to present a selected image for annotation / updating.
+    """
 
     res = db.session.query(Images)
+    image_uuid = uuid.UUID(image_uuid).hex
+
     image = res.filter(cast(Images.uuid, sqlalchemy.String) == image_uuid).first()
+
+    
+    # If current user is logged in, include current caption in presentation
+    annotation: Optional[str] = None
+    if current_user.is_authenticated:
+        user_image = db.session.query(UserImages
+                                      ).filter(cast(UserImages.image_uuid, sqlalchemy.String) == image_uuid
+                                      ).filter(UserImages.user_id == current_user.id
+                                      ).first()
+        if user_image:
+            annotation = user_image.annotation
 
     return render_template("image.html",
                            current_user=current_user,
                            image=image,
+                           annotation=annotation,
         )
 
 @main.route("/image/<image_uuid>", methods=["POST"])
 @login_required
 def save_image(image_uuid=None):
+    """
+    Route to post a new / updated annotation to the database.
+    """
 
     image_uuid = uuid.UUID(image_uuid)
     annotation = request.form.get("newDescription")
