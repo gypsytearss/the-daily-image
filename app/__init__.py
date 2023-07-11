7ba1fbcd6634b83e6e06e14b6db5210541a48e03
Colin Rennie <colinmatthew@gmail.com>
2023-07-11 19:26:10 -0400

    Add timer to images page

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-from .models import User
-
 db = SQLAlchemy()
 
 
@@ -29,6 +27,8 @@ def create_app() -> Flask:
     login_manager = LoginManager()
     login_manager.login_view = 'user.login'
     login_manager.init_app(app)
+
+    from .models import User
 
     @login_manager.user_loader
     def load_user(user_id):
